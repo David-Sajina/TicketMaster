ae321487a71a50bfff9c710e9df1167e37cde1b1
kresimirspehar <kresimirspehar5@gmail.com>
2022-07-14 17:40:29 +0200

    little fixes, signup and login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,69 +1,71 @@
 <template>
-	<v-content>
-		<v-container fluid fill-height>
-			<v-layout align-center justify-center>
-				<v-flex xs12 sm8 md4>
-					<v-card class="elevation-12">
-						<v-toolbar dark color="#616a76">
-							<v-toolbar-title>Login</v-toolbar-title>
-						</v-toolbar>
-						<v-card-text>
-							<v-form>
-								<v-text-field
-									prepend-icon="mdi-account"
-									name="login"
-									label="Login"
-									v-model="username"
-									type="text"
-								></v-text-field>
-								<v-text-field
-									id="password"
-									prepend-icon="mdi-lock"
-									name="password"
-									label="Password"
-									v-model="password"
-									type="password"
-								></v-text-field>
-							</v-form>
-						</v-card-text>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<!-- to="/" -->
-							<v-btn color="primary" @click="getTruth"
-								>Login<v-icon>mdi-check-circle-outline</v-icon></v-btn
-							>
-						</v-card-actions>
-					</v-card>
-				</v-flex>
-			</v-layout>
-		</v-container>
-	</v-content>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="#616a76">
+              <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  prepend-icon="mdi-account"
+                  name="login"
+                  label="Username"
+                  v-model="username"
+                  type="text"
+                  color="blue-grey darken-4"
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  v-model="password"
+                  type="password"
+                  color="blue-grey darken-4"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <!-- to="/" -->
+              <v-btn color="#bfff00" @click="getTruth"
+                >Login<v-icon>mdi-check-circle-outline</v-icon></v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-	import axios from "axios";
-	export default {
-		name: "Login",
-		data() {
-			return {
-				username: "",
-				password: "",
-			};
-		},
-		methods: {
-			async getTruth() {
-				try {
-					const temp = await axios.post(`http://localhost:5000/login`, {
-						username: this.username,
-						password: this.password,
-					});
-					console.log(temp.data);
-				} catch (error) {
-					console.log(error);
-				}
-			},
-		},
-	};
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    async getTruth() {
+      try {
+        const temp = await axios.post(`http://localhost:5000/login`, {
+          username: this.username,
+          password: this.password,
+        });
+        console.log(temp.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,78 +1,81 @@
 <template>
-	<v-content>
-		<v-container fluid fill-height>
-			<v-layout align-center justify-center>
-				<v-flex xs12 sm8 md4>
-					<v-card class="elevation-12">
-						<v-toolbar dark color="#616a76">
-							<v-toolbar-title>Sign up</v-toolbar-title>
-						</v-toolbar>
-						<v-card-text>
-							<v-form>
-								<v-text-field
-									prepend-icon="mdi-email"
-									name="email"
-									label="Email"
-									v-model="email"
-									type="email"
-								></v-text-field>
-								<v-text-field
-									prepend-icon="mdi-account"
-									name="login"
-									label="Login"
-									v-model="username"
-									type="text"
-								></v-text-field>
-								<v-text-field
-									id="password"
-									prepend-icon="mdi-lock"
-									name="password"
-									label="Password"
-									v-model="password"
-									type="password"
-								></v-text-field>
-							</v-form>
-						</v-card-text>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<!-- to="/" -->
-							<v-btn color="primary" @click="register"
-								>Login<v-icon>mdi-check-circle-outline</v-icon></v-btn
-							>
-						</v-card-actions>
-					</v-card>
-				</v-flex>
-			</v-layout>
-		</v-container>
-	</v-content>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="#616a76">
+              <v-toolbar-title>Sign up</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  prepend-icon="mdi-email"
+                  name="email"
+                  label="Email"
+                  v-model="email"
+                  type="email"
+                  color="blue-grey darken-4"
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="mdi-account"
+                  name="login"
+                  label="Username"
+                  v-model="username"
+                  type="text"
+                  color="blue-grey darken-4"
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                  v-model="password"
+                  type="password"
+                  color="blue-grey darken-4"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <!-- to="/" -->
+              <v-btn color="#bfff00" @click="register"
+                >Login<v-icon>mdi-check-circle-outline</v-icon></v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-	import axios from "axios";
-	export default {
-		name: "Signup",
-		data() {
-			return {
-				username: "",
-				password: "",
-				email: "",
-			};
-		},
-		methods: {
-			async register() {
-				try {
-					const temp = await axios.post(`http://localhost:5000/register`, {
-						username: this.username,
-						password: this.password,
-						email: this.email,
-					});
-					console.log("Valek the god", temp);
-				} catch (error) {
-					alert(error.response.data.error);
-				}
-			},
-		},
-	};
+import axios from "axios";
+export default {
+  name: "Signup",
+  data() {
+    return {
+      username: "",
+      password: "",
+      email: "",
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        const temp = await axios.post(`http://localhost:5000/register`, {
+          username: this.username,
+          password: this.password,
+          email: this.email,
+        });
+        console.log("Valek the god", temp);
+      } catch (error) {
+        alert(error.response.data.error);
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
