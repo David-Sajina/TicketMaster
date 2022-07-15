6df35557449c2f17235b49d43d6ec2121f29dbaa
kresimirspehar <kresimirspehar5@gmail.com>
2022-07-15 11:53:48 +0200

    header of ticket

--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="ticket">
+    <header-ticket />
     <q-a />
   </div>
 </template>
 <script>
 import QA from "@/components/QA.vue";
+import HeaderTicket from "../components/HeaderTicket.vue";
+
 export default {
-  name: "Ticket",
+  name: "About",
   components: {
     QA,
+    HeaderTicket,
   },
 };
 </script>
