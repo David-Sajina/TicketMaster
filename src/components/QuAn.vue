2ea14f3d90b729135c488fbc2f24532764069465
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-08-30 22:15:01 +0200

    Show questions and answers full cool (:

---
+++ src/components/QuAn.vue
@@ -0,0 +1,31 @@
+<template>
+	<div style="width: 90%" class="mx-auto">
+		<v-expansion-panels
+			:focusable="true"
+			:tile="true"
+			:hover="true"
+			class="pt-1 mt-2"
+		>
+			<v-expansion-panel>
+				<v-expansion-panel-header
+					><b>{{ question }}</b></v-expansion-panel-header
+				>
+				<v-expansion-panel-content class="mt-3">
+					<i>{{ answer }}</i>
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+		</v-expansion-panels>
+	</div>
+</template>
+<script>
+	export default {
+		name: "QuAn",
+		props: {
+			question: String,
+			answer: String,
+		},
+		data: () => ({}),
+		methods: {},
+		mounted() {},
+	};
+</script>
--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,24 +1,27 @@
 <template>
-	<div class="ticket" v-if="lista.length > 0">
-		<q-a
-			v-for="l in lista"
-			:key="l._id"
-			:question="l.question"
-			:answer="l.answer"
-		/>
-	</div>
+	<v-app>
+		<div class="ticket" style="margin-top: 15px" v-if="lista.length > 0">
+			<qu-an
+				v-for="l in lista"
+				:key="l._id"
+				:question="l.question"
+				:answer="l.answer"
+			/></div
+	></v-app>
 </template>
 
 <script>
 	import HeaderTicket from "../components/HeaderTicket.vue";
 	import QA from "../components/QA.vue";
 	import axios from "axios";
+	import QuAn from "../components/QuAn.vue";
 
 	export default {
 		name: "Questions",
 		components: {
 			HeaderTicket,
 			QA,
+			QuAn,
 		},
 		data() {
 			return {
