4935e4a3cfbd1bda66211a689c1e333bb97bf083
kresimirspehar <kresimirspehar5@gmail.com>
2022-09-03 20:54:56 +0200

    fixes

--- src/components/HeaderTicket.vue
+++ src/components/HeaderTicket.vue
@@ -55,71 +55,6 @@
             ></v-text-field>
           </v-expansion-panel-content>
         </v-expansion-panel>
-
-        <v-expansion-panel>
-          <v-expansion-panel-header v-slot="{ open }">
-            <v-row no-gutters>
-              <v-col cols="4"> Ticket starting day: </v-col>
-              <v-col cols="8" class="text--secondary">
-                <v-fade-transition leave-absolute>
-                  <span v-if="open">Starting time ?</span>
-                  <v-row v-else no-gutters style="width: 100%">
-                    <v-col cols="6">
-                      Start date: {{ listaTicket.start || "Not set" }}
-                    </v-col>
-                  </v-row>
-                </v-fade-transition>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-row justify="space-around" no-gutters>
-              <v-col cols="3">
-                <v-menu
-                  ref="startMenu"
-                  :close-on-content-click="false"
-                  :return-value.sync="ticket.start"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="ticket.start"
-                      label="Start date"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="date"
-                    :reactive="reactive"
-                    :min="nowDate"
-                    no-title
-                    scrollable
-                  >
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="$refs.startMenu.isActive = false"
-                    >
-                      Cancel
-                    </v-btn>
-                    <v-btn
-                      text
-                      color="primary"
-                      @click="$refs.startMenu.save(date)"
-                    >
-                      OK
-                    </v-btn>
-                  </v-date-picker>
-                </v-menu>
-              </v-col>
-            </v-row>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
       </v-expansion-panels>
       <div class="text-center">
         <v-btn class="ma-2" outlined color="#404040" @click="addAndUpdate">
@@ -174,7 +109,6 @@ export default {
   name: "HeaderTicket",
   components: { QA },
   data: () => ({
-    date: null,
     lista: [],
     listaTicket: null,
     loading: true,
@@ -182,14 +116,9 @@ export default {
     ticket: {
       name: "",
       location: "",
-      start: null,
       question: "",
       answer: "",
     },
-    nowDate: new Date().toISOString().slice(0, 10),
-    date: new Date(),
-    date: new Date().toISOString().substr(0, 10),
-    reactive: false,
   }),
   computed: {
     ...mapGetters({ user: "user" }),
@@ -201,7 +130,6 @@ export default {
           user: this.user.email,
           name: this.ticket.name,
           location: this.ticket.location,
-          start: this.ticket.start,
         });
         this.getQA();
       } catch (error) {
@@ -241,7 +169,6 @@ export default {
             user: this.user,
             name: this.ticket.name,
             location: this.ticket.location,
-            start: this.ticket.start,
           });
         } else {
           console.log("test");
@@ -250,7 +177,6 @@ export default {
             {
               name: this.ticket.name,
               location: this.ticket.location,
-              start: this.ticket.start,
             }
           );
         }
@@ -268,7 +194,6 @@ export default {
         if (lista1 && lista1.data && lista1.data.name) {
           this.hasData = true;
           this.ticket.name = lista1.data.name;
-          this.ticket.start = lista1.data.start;
           this.ticket.location = lista1.data.location;
         }
         this.loading = false;
--- src/components/QA.vue
+++ src/components/QA.vue
@@ -1,138 +1,135 @@
 <template>
-	<v-container>
-		<v-card>
-			<div class="d-flex">
-				<v-expansion-panels>
-					<v-expansion-panel>
-						<v-expansion-panel-header
-							><v-text-field
-								v-model="newQuestion"
-								:disabled="disabled"
-								label="Pitanje?"
-							></v-text-field>
-						</v-expansion-panel-header>
+  <v-container>
+    <v-card>
+      <div class="d-flex">
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header
+              ><v-text-field
+                v-model="newQuestion"
+                :disabled="disabled"
+                label="Pitanje?"
+              ></v-text-field>
+            </v-expansion-panel-header>
 
-						<v-expansion-panel-content>
-							<v-text-field
-								v-model="newAnswer"
-								:disabled="disabled"
-								label="Odgovor"
-							></v-text-field>
-							<v-row>
-								<v-btn class="ma-2" color="#bfff00" small>
-									<v-icon color="#616a76" @click="edit" small>
-										mdi-pencil
-									</v-icon>
-								</v-btn>
-								<v-btn v-if="save" class="ma-2" color="#bfff00" small>
-									<v-icon color="#616a76" @click="saveEdit"> mdi-check </v-icon>
-								</v-btn>
-								<v-spacer></v-spacer>
-								<v-btn class="ma-2" color="#616a76" small>
-									<v-icon @click.stop="dialog = true" color="#bfff00" small>
-										mdi-delete
-									</v-icon>
-								</v-btn>
-							</v-row>
-							<v-dialog v-model="dialog" color="#616a76" max-width="290">
-								<v-card>
-									<v-card-title class="text-h5" color="#616a76">
-										DELETE
-									</v-card-title>
-									<v-card-text>
-										Are you sure you want to delete this ticket question?
-									</v-card-text>
+            <v-expansion-panel-content>
+              <v-text-field
+                v-model="newAnswer"
+                :disabled="disabled"
+                label="Odgovor"
+              ></v-text-field>
+              <v-row>
+                <v-btn class="ma-2" color="#bfff00" small>
+                  <v-icon color="#616a76" @click="edit" small>
+                    mdi-pencil
+                  </v-icon>
+                </v-btn>
+                <v-btn v-if="save" class="ma-2" color="#bfff00" small>
+                  <v-icon color="#616a76" @click="saveEdit"> mdi-check </v-icon>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn class="ma-2" color="#616a76" small>
+                  <v-icon @click.stop="dialog = true" color="#bfff00" small>
+                    mdi-delete
+                  </v-icon>
+                </v-btn>
+              </v-row>
+              <v-dialog v-model="dialog" color="#616a76" max-width="290">
+                <v-card>
+                  <v-card-title class="text-h5" color="#616a76">
+                    DELETE
+                  </v-card-title>
+                  <v-card-text>
+                    Are you sure you want to delete this ticket question?
+                  </v-card-text>
 
-									<v-card-actions>
-										<v-spacer></v-spacer>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
 
-										<v-btn color="green darken-1" text @click="dialog = false">
-											Disagree
-										</v-btn>
+                    <v-btn color="green darken-1" text @click="dialog = false">
+                      Disagree
+                    </v-btn>
 
-										<v-btn color="green darken-1" text @click="deleteThis">
-											Agree
-										</v-btn>
-									</v-card-actions>
-								</v-card>
-							</v-dialog>
-						</v-expansion-panel-content>
-					</v-expansion-panel>
-				</v-expansion-panels>
-			</div></v-card
-		>
-	</v-container>
+                    <v-btn color="green darken-1" text @click="deleteThis">
+                      Agree
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div></v-card
+    >
+  </v-container>
 </template>
 <script>
-	import { mapGetters } from "vuex";
-	import axios from "axios";
+import { mapGetters } from "vuex";
+import axios from "axios";
 
-	export default {
-		name: "QA",
-		props: {
-			question: String,
-			answer: String,
-			id: String,
-		},
-		data: () => ({
-			readonly: false,
-			disabled: true,
-			save: false,
-			newQuestion: "",
-			newAnswer: "",
-			dialog: false,
-		}),
+export default {
+  name: "QA",
+  props: {
+    question: String,
+    answer: String,
+    id: String,
+  },
+  data: () => ({
+    readonly: false,
+    disabled: true,
+    save: false,
+    newQuestion: "",
+    newAnswer: "",
+    dialog: false,
+  }),
 
-		methods: {
-			async setQA() {
-				this.newQuestion = this.question;
-				this.newAnswer = this.answer;
-			},
+  methods: {
+    async setQA() {
+      this.newQuestion = this.question;
+      this.newAnswer = this.answer;
+    },
 
-			edit() {
-				this.disabled = !this.disabled;
-				this.save = !this.save;
-			},
-			async deleteThis() {
-				await axios.delete(`http://localhost:5000/ticket/${this.id}`);
-				this.dialog = false;
-				this.comp = !this.comp;
-				this.$toast.info("Successfully deleted", {
-					position: "bottom-center",
-					timeout: 2966,
-					closeOnClick: true,
-					pauseOnFocusLoss: true,
-					pauseOnHover: true,
-					draggable: true,
-					draggablePercent: 0.6,
-					showCloseButtonOnHover: false,
-					hideProgressBar: true,
-					closeButton: "button",
-					icon: true,
-					rtl: false,
-				});
-			},
+    edit() {
+      this.disabled = !this.disabled;
+      this.save = !this.save;
+    },
+    async deleteThis() {
+      await axios.delete(`http://localhost:5000/ticket/${this.id}`);
+      this.dialog = false;
+      this.comp = !this.comp;
+      this.$toast.info("Successfully deleted", {
+        position: "bottom-center",
+        timeout: 2966,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: "button",
+        icon: true,
+        rtl: false,
+      });
+    },
 
-			async saveEdit() {
-				this.disabled = !this.disabled;
-				this.save = !this.save;
-				const res = await axios.patch(
-					`http://localhost:5000/ticket/${this.id}`,
-					{
-						newQuestion: this.newQuestion,
-						newAnswer: this.newAnswer,
-					}
-				);
-				console.log(res.data);
-			},
-		},
+    async saveEdit() {
+      this.disabled = !this.disabled;
+      this.save = !this.save;
+      const res = await axios.patch(`http://localhost:5000/ticket/${this.id}`, {
+        newQuestion: this.newQuestion,
+        newAnswer: this.newAnswer,
+      });
+      console.log(res.data);
+    },
+  },
 
-		computed: {
-			...mapGetters({ user: "user" }),
-		},
+  computed: {
+    ...mapGetters({ user: "user" }),
+  },
 
-		mounted() {
-			this.setQA();
-		},
-	};
+  mounted() {
+    this.setQA();
+  },
+};
 </script>
