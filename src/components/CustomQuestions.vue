c1f5a630d20faf6541cf962bb9de136a730b1807
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-09-08 21:02:03 +0200

    Answer custom questions, 50% done

---
+++ src/components/CustomQuestions.vue
@@ -0,0 +1,34 @@
+<template>
+	<div style="width: 90%" class="mx-auto">
+		<v-expansion-panels
+			:focusable="true"
+			:tile="true"
+			:hover="true"
+			class="pt-1 mt-2"
+		>
+			<v-expansion-panel>
+				<v-expansion-panel-header
+					><b>{{ question }}</b></v-expansion-panel-header
+				>
+				<v-expansion-panel-content class="mt-3">
+					Answer
+					<v-text-field></v-text-field>
+					<v-btn class="mx-2" fab dark small color="green">
+						<v-icon dark> mdi-check </v-icon>
+					</v-btn>
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+		</v-expansion-panels>
+	</div>
+</template>
+<script>
+	export default {
+		name: "CustomQuestions",
+		props: {
+			question: String,
+		},
+		data: () => ({}),
+		methods: {},
+		mounted() {},
+	};
+</script>
--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="ticket">
+	<div class="ticket" v-if="user">
 		<v-tabs background-color="transparent" color="basil" grow>
 			<v-tab> Ticket </v-tab>
 			<v-tab> Questions </v-tab>
@@ -7,22 +7,50 @@
 				<header-ticket />
 			</v-tab-item>
 			<v-tab-item>
-				<v-app>{{ user.username }} </v-app>
+				<v-app>
+					<custom-questions
+						v-for="l in lista"
+						:key="l._id"
+						:question="l.question"
+					/>
+				</v-app>
 			</v-tab-item>
 		</v-tabs>
 	</div>
 </template>
 <script>
 	import HeaderTicket from "../components/HeaderTicket.vue";
-	import { mapMutations, mapGetters, mapActions } from "vuex";
+	import CustomQuestions from "../components/CustomQuestions.vue";
+	import { mapGetters } from "vuex";
+	import axios from "axios";
 
 	export default {
 		name: "About",
 		components: {
 			HeaderTicket,
+			CustomQuestions,
+		},
+		data: () => ({
+			lista: [],
+		}),
+		methods: {
+			async getQ() {
+				try {
+					let lista1 = await axios.get(
+						`http://localhost:5000/q/${this.user.email}`
+					);
+					this.lista = lista1.data;
+					console.log(lista1.data);
+				} catch (error) {
+					console.log(error);
+				}
+			},
 		},
 		computed: {
 			...mapGetters({ user: "user" }),
+		},
+		created() {
+			this.getQ();
 		},
 	};
 </script>
