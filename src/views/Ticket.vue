b1a1c1bd48348768b0d84444e075529c27e8ef33
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-09-08 20:37:09 +0200

    added tabs

--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -1,15 +1,28 @@
 <template>
 	<div class="ticket">
-		<header-ticket />
+		<v-tabs background-color="transparent" color="basil" grow>
+			<v-tab> Ticket </v-tab>
+			<v-tab> Questions </v-tab>
+			<v-tab-item>
+				<header-ticket />
+			</v-tab-item>
+			<v-tab-item>
+				<v-app>{{ user.username }} </v-app>
+			</v-tab-item>
+		</v-tabs>
 	</div>
 </template>
 <script>
 	import HeaderTicket from "../components/HeaderTicket.vue";
+	import { mapMutations, mapGetters, mapActions } from "vuex";
 
 	export default {
 		name: "About",
 		components: {
 			HeaderTicket,
+		},
+		computed: {
+			...mapGetters({ user: "user" }),
 		},
 	};
 </script>
